c708e0f835b6f67323a27d2c2db76b0e32a0c4d2
marina-fdz <marina.fdz.caceres@gmail.com>
2024-07-29 20:41:35 +0200

    add service

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class ByCapitalPageComponent {
 
+  searchByCapital( term: string){
+    console.log('Desde ByCapitalPage');
+    console.log({ term });
+
+  }
+
 }
